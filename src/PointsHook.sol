11bcf3d7b149791e8ef1f7b793e055408d2a0fb1
Daksha <dakshaaeer@gmail.com>
2025-11-01 19:01:10 +0530

    update formatting

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -2,45 +2,37 @@ pragma solidity 0.8.26;
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
 import {ERC1155} from "solmate/src/tokens/ERC1155.sol";
- 
+
 import {Currency} from "v4-core/types/Currency.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {PoolId} from "v4-core/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
- 
+
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
- 
+
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 contract PointsHook is BaseHook, ERC1155 {
-    constructor(
-        IPoolManager _manager
-    ) BaseHook(_manager) {}
+    constructor(IPoolManager _manager) BaseHook(_manager) {}
 
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: false,
-                beforeAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterAddLiquidity: false,
-                afterRemoveLiquidity: false,
-                beforeSwap: false,
-                afterSwap: true,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: false,
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterAddLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: false,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
 
     function uri(uint256) public view virtual override returns (string memory) {
@@ -71,12 +63,10 @@ contract PointsHook is BaseHook, ERC1155 {
         if (currentTotalVolume > 10 ether) {
             //Tier 1: 20% rate
             pointsForSwap = ethSpendAmount / 5;
-        }
-        else if (currentTotalVolume > 5 ether) {
+        } else if (currentTotalVolume > 5 ether) {
             //Tier 2: 10% rate
             pointsForSwap = ethSpendAmount / 10;
-        }
-        else {
+        } else {
             //Tier 3: 5% rate
             pointsForSwap = ethSpendAmount / 20;
         }
@@ -87,11 +77,7 @@ contract PointsHook is BaseHook, ERC1155 {
         return (this.afterSwap.selector, 0);
     }
 
-    function _assignPoints(
-        PoolId poolId,
-        bytes calldata hookData,
-        uint256 points
-    ) internal {
+    function _assignPoints(PoolId poolId, bytes calldata hookData, uint256 points) internal {
         // if no hookdata passed, no points assigned
         if (hookData.length == 0) return;
 
